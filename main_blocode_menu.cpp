57f0f7fcb9917b0639dcac321cbcf810094023b6
Kdemx <63835107+pedro408@users.noreply.github.com>
2023-06-07 11:39:06 -0300

    Primeira tentativa de criação de app
    Criação do app blocode. primeira tentativa.

---
+++ main_blocode_menu.cpp
@@ -0,0 +1,47 @@
+#include <windows.h>
+
+// Função de callback para processar mensagens da janela
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_COMMAND:
+            if (LOWORD(wParam) == 1) // Verifica se o ID do controle é 1
+            {
+                MessageBox(hwnd, L"Botão pressionado!", L"Exemplo", MB_OK);
+            }
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Registrar a classe da janela
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"MainWindowClass";
+    RegisterClass(&wc);
+
+    // Criar a janela
+    HWND hwnd = CreateWindowEx(0, L"MainWindowClass", L"Exemplo de Botão", WS_OVERLAPPEDWINDOW,
+                               CW_USEDEFAULT, CW_USEDEFAULT, 500, 300,
+                               NULL, NULL, hInstance, NULL);
+
+    // Criar o botão
+    HWND btn = CreateWindow(L"BUTTON", L"Pressione-me", WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+                            10, 10, 120, 40, hwnd, (HMENU)1, hInstance, NULL);
+
+    // Mostrar a janela
+    ShowWindow(hwnd, nCmdShow);
+
+    // Loop de mensagens
+    MSG msg = {};
